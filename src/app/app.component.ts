ba23fcfbf42ade5a790647dfb5cf9d342b083084
Gonick Nalwa <gonick.nalwa@jungleworks.co>
2020-07-28 18:22:29 +0530

    task complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+interface IBox {
+  id: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'box-generator';
+
+  //counter to serve as unique id
+  counter: number = 0
+  //list of boxes
+  boxes: IBox[] = [];
+
+  activeBox: number = -1;
+
+  isKeyboardEnabled: boolean = true;
+
+  onAddNewBox() {
+    this.counter++;
+    this.boxes.push({ id: this.counter });
+  }
+
+  onBoxSelect(box: IBox) {
+    this.activeBox = box.id
+  }
+
+  onDelete(index: number) {
+    this.boxes.splice(index, 1);
+  }
+
+  //to optimize UI and reduce dom manipulations on change
+  trackByFn(index: number, item: IBox) {
+    return item.id;
+  }
+
+  onKeyboardControlChange(e: Event) {
+    const isChecked = (e.target as HTMLInputElement).checked
+    this.isKeyboardEnabled = isChecked;
+  }
 }
---
+++ src/app/box/box/box.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit, Input, Renderer2, ChangeDetectionStrategy, OnDestroy, ChangeDetectorRef, ElementRef, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BoxComponent implements OnInit, OnDestroy {
+  private _keyboardEvents: boolean;
+
+  //to sent event to parent when delete key is pressed
+  @Output('delete') onDelete: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  @Input('id') boxId: number;
+  @Input('active') active:boolean;
+
+  @Input('enableKeyboardEvents') set keyboardEvents(val: boolean) {
+    this._keyboardEvents = !!val;
+    //enable or disable keyboard listeners on input change
+    if (val) this.listenToKeyboardEvent();
+    else this.removeListener();
+  }
+
+  get keyboardEvents() {
+    return this._keyboardEvents
+  }
+
+  left: number = 0;
+  top: number = 0;
+
+  subs: Function;//to unsubscibe listeners
+
+  max = { x: 0, y: 0 } //bounding box values 
+
+  constructor(private renderer: Renderer2, private ref: ChangeDetectorRef, private el: ElementRef) {
+  }
+
+  ngOnInit() {
+    let parent = { x: 0, y: 0 }, child = { x: 0, y: 0 };
+    parent.x = (this.el.nativeElement as HTMLElement).parentElement.clientWidth;
+    parent.y = (this.el.nativeElement as HTMLElement).parentElement.clientHeight;
+
+    child.x = this.el.nativeElement.firstElementChild.offsetWidth;
+    child.y = this.el.nativeElement.firstElementChild.offsetHeight;
+
+    //to ensure all the boxes stay within the fence during movement
+    this.max.x = parent.x - child.x;
+    this.max.y = parent.y - child.y;
+  }
+
+  listenToKeyboardEvent() {
+    this.removeListener();
+    this.subs = this.renderer.listen('document', 'keydown', e => {
+      window.requestAnimationFrame(() => { //animation frame for smooth UX
+        this.onKeyPress(e);
+        this.ref.detectChanges()
+      })
+    })
+  }
+
+  private onKeyPress(e) {
+    switch (e.code) {
+      case "KeyW":
+        this.top <= 0 ? 0 : this.top--;
+        break;
+      case "KeyA":
+        this.left <= 0 ? 0 : this.left--;
+        break;
+      case "KeyS":
+        this.top >= this.max.y ? this.max.y : this.top++;
+        break;
+      case "KeyD":
+        this.left >= this.max.x ? this.max.x : this.left++;
+        break;
+      case "Delete":
+        this.onDelete.emit(true);
+        break;
+    }
+  }
+
+  private removeListener() {
+    if (this.subs)
+      this.subs();
+  }
+
+  ngOnDestroy() {
+    this.removeListener();
+  }
+
+}
